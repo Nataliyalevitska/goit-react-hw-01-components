a1fa8280fe6ae93024d3a73ccc71907805310c5a
natalilevitska <nataliyalevi579@gmail.com>
2022-05-30 11:21:32 +0300

    add homepage

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import s from './Profile.module.css';
+// import user from './user.json';
+
+const Profile = ({ user }) => {
+  const { avatar, username, tag, location, stats } = user;
+  return (
+    <div className={s.Profilewrap}>
+      <div className={s.description}>
+        <img src={avatar} alt="User avatar" className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.colorTitle}>{'@' + tag}</p>
+        <p className={s.colorTitle}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li className={s.Profileitem}>
+          <span className={(s.label, s.colorTitle)}>Followers</span>
+          <span className="quantity"> {stats.followers}</span>
+        </li>
+        <li className={s.Profileitem}>
+          <span className={(s.label, s.colorTitle)}>Views</span>
+          <span className="quantity"> {stats.views}</span>
+        </li>
+        <li className={s.Profileitem}>
+          <span className={(s.label, s.colorTitle)}>Likes</span>
+          <span className="quantity"> {stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
